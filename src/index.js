1c44663e1c698177a55d282f6ee5a167548f0d4f
BbreeSwn <suwanan.cs@hotmail.com>
2024-04-29 16:09:38 +0700

    input and select

--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
+// function App (){
+//   const [data, setData] = React.useState('');
+//   // const data = 'Codecamp';
+  
+//   const handleChange = (event) => {
+//     console.log(event.target.value);
+//     setData(event.target.value);
+//   };
+
+//   return <input value={data} onChange={handleChange} />;
+// }
+
 function App (){
-  return <div>Hello React</div>;
+  const [selected , setSelected] = React.useState('');
+  // const data = 'Codecamp';
+  
+  const handleSelect = (event) => {
+    // console.log(event.target.value);
+    setSelected(event.target.value);
+  };
+
+  return (
+    <select onChange={handleSelect}>
+      <option value='1'>option-1</option>
+      <option value='2'>option-2</option>
+      <option value='3'>option-3</option>
+    </select>
+  );
 }
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
