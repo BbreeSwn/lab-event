c801d9f40314819675a87d5d4b9cd4a24a50cd95
BbreeSwn <suwanan.cs@hotmail.com>
2024-04-30 09:34:25 +0700

    finish-lab3

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom/client';
 
 
 function App (){
-    const handleSelect = (event) => {
-        console.log(event.taget.value);
-    };
+    const handleCheck = (event) => {
+        console.log(event.target.name)
+        console.log(event.target.name)
+        console.log(event.target.value)
+    }
+    return (
+        <>
+            <input type='checkbox' name="phonebrand" id='phone-1' value='Sumsung' onChange={handleCheck} />
+            <label htmlFor='phone-1'>Samsung</label>
+            <input type='checkbox' name="phonebrand" id='phone-2' value='Nokia' onChange={handleCheck} />
+            <label htmlFor='phone-2'>Nokia</label>
+            <input type='checkbox' name="phonebrand" id='phone-3' value='Huawei' onChange={handleCheck} />
+            <label htmlFor='phone-3'>Huawei</label>
+            <input type='checkbox' name="phonebrand" id='phone-4' value='Iphone' onChange={handleCheck} />
+            <label htmlFor='phone-4'>Iphone</label>
+            
+        </>
+    )
+
 }
-return (
-    <select>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-    </select>
-)
 
     // //render
     const root = ReactDOM.createRoot(document.getElementById('root'));
